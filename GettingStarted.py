2810f7e0b3227bdd19e3aadedf3cf9ebb620c91e
FelixQuast <quast.felix@gmail.com>
2020-10-26 17:45:06 +0100

    test commit

--- GettingStarted.py
+++ GettingStarted.py
@@ -1,8 +1,34 @@
-print("hello world!")
-a = 600 + 60 + 6
+# Strings
 
+str = "Das ist ein String!"
+print(str)
+
+a = str.upper()
 print(a)
 
-print(type(a))
+b = str.lower()
+print(b)
 
-b = test
+c = str[0]
+print(c)
+
+d = str[4:7]
+print(d)
+
+e = str[4:]
+print(e)
+
+f = str.split()
+print(f, type(f))
+
+name = "hans"
+alter = 34.123456789
+print("%s ist %r Jahre alt" %(name, alter))
+
+neuername = "Dieter"
+flieskomma = 66.852
+print("%s ist %40.10f Jahre alt" %(neuername, 66.852))
+
+liste = ("hallo", 3.14, 35)
+print("erstes: %s, zweites: %r, drittes: %r" %(liste))
+print(type(liste))
