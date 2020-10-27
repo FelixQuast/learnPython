e3200a6eec0258c0c34382e7f4c13c050a12e68f
FelixQuast <quast.felix@gmail.com>
2020-10-27 09:47:11 +0100

    new

--- GettingStarted.py
+++ GettingStarted.py
@@ -1,5 +1,6 @@
 # Strings
-
+import math
+pi = math.pi
 str = "Das ist ein String!"
 print(str)
 
@@ -29,9 +30,9 @@ neuername = "Dieter"
 flieskomma = 66.852
 print("%s ist %40.10f Jahre alt" %(neuername, 66.852))
 
-liste = ("hallo", 3.14, 35)
-print("erstes: %s, zweites: %r, drittes: %r" %(liste))
-print(type(liste))
+tupel = ("hallo", 3.14, 35)
+print("erstes: %s, zweites: %r, drittes: %r" % (tupel))
+print(type(tupel))
 
 g = "hallo"
 h = "test"
@@ -41,4 +42,45 @@ print('test 1: {}, test 2: {}, test 3: {}'.format(g, h, i))     #die klammern bl
 j = "Stuhl"
 k = "1000000€"
 print('Zu Verkaufen: {Ware}, Preis: {Preis}'.format(Ware = j, Preis = k))
+#oder oder oder oder oder oder
+print('Zu Verkaufen: {Ware}, Preis: {Preis}'.format(Ware = "Tisch", Preis = "50"))
+
+liste = ["String", pi, pi**2]
+print(liste)
+liste = liste + [666]
+print(liste)
+liste.append("test")
+print(liste)
+geklaut = liste.pop(1)
+print(liste)
+print(geklaut)
+
+neue_liste = ["a", "d", "c", "b"]
+print(neue_liste)
+neue_liste.sort()
+print(neue_liste)
+
+vector1 = [1, 2, 3]
+vector2 = [4, 5, 6]
+vector3 = [7, 8, 9]
+
+matrix = [vector1, vector2, vector3]
+print(vector1)
+print(vector2)
+print(vector3)
+print(matrix)
+
+test = matrix[1]
+print(test)
+print(type(test))
+test2  = repr(test)
+print(test2)
+print(type(test2))
+
+#test_string = "".join([str(_) for _ in a])
+
+
+#print("probieren" + test_string)
+
+
 
